9e8902ea7382ee105ad1e983b9f07a91a7e8968e
Phil DeMonaco <pdemon@gmail.com>
2017-12-04 17:21:21 -0500

    MAC Gen Script
    Some janky shit that creates a mac address from the "generate" space
    with those two bits. It doesn't format it quite right for the cisco
    style output variant.

---
+++ scripts/mac_gen.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+RAW_MAC=$(xxd -p -l 6 /dev/urandom \
+| awk '
+{
+    and_num = strtonum("0xfeffffffffff");
+    or_num = strtonum("0x020000000000");
+    numstr = "0x"$1;
+    num = strtonum(numstr);
+    mod_num = and( or( num, or_num ), and_num );
+    printf "%0x\n", mod_num;
+}')
+
+SEP_MAC=$(echo "${RAW_MAC}" | sed 's/.\{2\}/&:/g')
+SEP_MAC="${SEP_MAC%?}"
+
+SEP_MAC_2=$(echo "${RAW_MAC}" | sed 's/.\{4\}/&./g')
+
+echo $RAW_MAC
+echo $SEP_MAC
+echo $SEP_MAC_2
