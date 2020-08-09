8dbb4ba7f3882e472ab29ef7dd3bc590d1363861
Philip DeMonaco <phil@demona.co>
2020-08-09 10:06:27 -0400

    feat: updated chroot script to be pretty
    Actually has a help, supports arguments, and does some other shit.
    
    Should make new machine setup a bit easier.

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -1,12 +1,117 @@
 #!/bin/bash
-if [[ $1 = "start" ]]
-then  
-  zpool import -f -o cachefile= -R /mnt/gentoo rpool
-  mount -t proc proc /mnt/gentoo/proc/
-  mount --rbind /dev /mnt/gentoo/dev
-  mount --rbind /sys /mnt/gentoo/sys
-elif [[ $1 = "stop" ]]
-then
-  umount -l /mnt/gentoo/proc /mnt/gentoo/dev /mnt/gentoo/sys
-  zpool export rpool
+
+BASENAME=$(basename "$0")
+
+# Usage --------------------------------------------
+USAGE="Trivial CHROOT script
+Usage: ${BASENAME} [-m MOUNT_POINT] -p POOL -a ACTION
+   or: ${BASENAME} [-h]
+
+Provides a simple mechanism to chroot into a new gentoo install environment
+which has a ZFS root.
+
+Mandatory Arguments:
+  -p POOL           Name of the ZPOOL which should be mounted at the root of the
+                    gentoo install
+
+  -a ACTION         One of two options, START which mounts all the shit and
+                    STOP which unmounts everything.
+
+Optional Arguments:
+  -m MOUNT_POINT      If specified, the script uses this as the mount target
+                      instead of '/mnt/gentoo'.
+
+  -h, --help          display this help and exit
+"
+
+#-------- Print Error Message -------------------------------------------------
+function echoerr()
+{
+    printf "%s\n" "$*" >&2;
+    exit 1
+}
+
+#-------- Parse Arguments -----------------------------------------------------
+# Shamelessly stolen from
+# https://medium.com/@Drew_Stokes/bash-argument-parsing-54f3b81a6a8f
+# Note that the variables in this function are intentionally globals
+function parse_arguments()
+{
+    # Collect parameters
+    PARAMS=""
+    while (( "$#" ));
+    do
+        case "$1" in
+            -h|--help)
+                # Print usage and quit
+                echo "${USAGE}"
+                exit 0
+                ;;
+            -a)
+                ACTION=$2
+                shift 2
+                ;;
+            -m)
+                MOUNT_POINT=$2
+                shift 2
+                ;;
+            -p)
+                POOL_NAME=$2
+                shift 2
+                ;;
+            --) # end argument parsing
+                shift
+                break
+                ;;
+            -*) # unsupported flags
+                echoerr "${BASENAME}: Unsupported flag ${1}"
+                ;;
+            *) # preserve positional arguments
+                PARAMS="$PARAMS $1"
+                shift
+                ;;
+        esac
+    done
+
+    # Check mandatory parameters
+    if [ -z "${ACTION}" ]; then
+        echoerr "${BASENAME}: ACTION is mandatory!"
+    fi
+
+    if [ -z "${POOL_NAME}" ]; then
+        echoerr "${BASENAME}: POOL_NAME is mandatory!"
+    fi
+
+    # Default optional param
+    if [ -z "${MOUNT_POINT}" ]; then
+        MOUNT_POINT="/mnt/gentoo"
+    fi
+}
+
+#======== Main control block ==================================================
+
+# Record the parameters - intentionally multiple words
+# shellcheck disable=SC2068
+parse_arguments $@
+
+if ! [ -d "${MOUNT_POINT}" ]; then
+    mkdir -p "${MOUNT_POINT}"
 fi
+
+case "${ACTION}" in
+    START)
+        zpool import -f -o cachefile= -R "${MOUNT_POINT}" "${POOL_NAME}"
+        mount -t proc proc "${MOUNT_POINT}/proc/"
+        mount --rbind /dev "${MOUNT_POINT}/dev/"
+        mount --rbind /sys "${MOUNT_POINT}/sys"
+        ;;
+    STOP)
+        umount -l /mnt/gentoo/proc /mnt/gentoo/dev /mnt/gentoo/sys
+        zpool export rpool
+        ;;
+esac
+
+# shellcheck disable=SC2016
+printf "To switch to the new environment:\n\t%s\n\t%s" \
+    "chroot ${MOUNT_POINT} /bin/bash" \
+    'source /etc/profile; env-update; export PS1="(chroot) $PS1"; cd'
